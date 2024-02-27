1a946a76aa5494e142a7d18478e1e85f0b644f0b
ujjwal <ujjwalsharma515@gmail.com>
2024-02-27 18:51:05 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import Dashboard from './modules/Dashboard';
+import Form from './modules/Form';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children,auth=false} ) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || false;
+  
+
+
+
+
+  if (!isLoggedIn && auth) {
+    return <Navigate to={'/users/sign_in'} />;
+  } else if (isLoggedIn && ['/users/sign_in', '/user/sign_up'].includes(window.location.pathname)) {
+    return <Navigate to={'/'} />;
+  }
+
+  return children;
+};
+
+function App() {
+   
+  return (
+   <div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute auth={true}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/sign_in"
+        element={
+          <ProtectedRoute>
+            <Form isSignInPage={true} />
+          </ProtectedRoute>
+        }
+      />
+       <Route
+        path="/users/sign_up"
+        element={
+         <ProtectedRoute>
+            <Form isSignInPage={false} /> </ProtectedRoute>
+        } 
+      />
+    </Routes>
+  
+   {/* <Routes>
+      <Route
+        path="/"
+        element={
+        
+            <Dashboard />
+         
+        }
+      />
+      <Route
+        path="/users/sign_in"
+        element={
+         
+            <Form isSignIn={true} />
+        
+        }
+      />
+       <Route
+        path="/users/sign_up"
+        element={
+        
+            <Form isSignIn={false} />
+        } 
+      />
+    </Routes> */}
+  
+    </div>
+  );
+}
+
+export default App;
