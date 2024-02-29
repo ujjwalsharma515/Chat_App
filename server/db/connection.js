2c8b092733863641cf071a901b489dda1a3778d4
ujjwal <ujjwalsharma515@gmail.com>
2024-02-29 23:06:26 +0530

    j

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://chatapp_admin:chatapp_admin@cluster0.1jhklwo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 ).then(()=>{
-    console.log("connection successful");
+    console.log("connection successful"); 
 }).catch((err)=>
 {
     console.log("not successfull");
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const io = require('socket.io')(8080, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chat-app-1-zqtl.onrender.com',
     }
 });
 
