e0fcc078657504eb13ac7fbe24581fd0b10c8cdb
ujjwal <ujjwalsharma515@gmail.com>
2024-03-09 12:19:05 +0530

    hj

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
 	const messageRef = useRef(null)
 
 	useEffect(() => {
-		setSocket(io('http://localhost:process.env.PORT'))
+		setSocket(io)
 	}, [])
 
 	useEffect(() => {
