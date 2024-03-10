025a02746f8fc60560f56e37e55e311bdd2adc35
ujjwal <ujjwalsharma515@gmail.com>
2024-03-10 20:08:48 +0530

    hi

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -36,11 +36,11 @@ const Form = ({isSignInPage = true}) => {
         }
     }
   return (
-    
+
     <div className="bg-light h-screen flex items-center justify-center">
         <div className=" bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
             <div className=" text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
-            <div className=" text-xl font-light mb-14">{isSignInPage ? 'Sign in to get explored' : 'please Sign in after sign up to get started'}</div>
+            <div className=" text-xl font-light mb-14">{isSignInPage ? 'Sign in to get explored' : 'Please sign in after sign up to get started'}</div>
 
             <form className="flex flex-col items-center w-full" onSubmit={(e) => handleSubmit(e)}>
             { !isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6 w-[75%]" value={data.fullName} onChange={(e) => setData({ ...data, fullName: e.target.value }) } /> }
