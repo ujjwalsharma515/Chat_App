bbd4d502c747a7dc64139b35168792324c650eb2
ujjwal <ujjwalsharma515@gmail.com>
2024-03-18 11:23:40 +0530

    del

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -56,7 +56,7 @@ const Dashboard = () => {
 	}, [])
 
 	const deleteConv = async (conversationId) => {
-		const res = await fetch(`https://chat-app-1-zqtl.onrender.com/deleteConv/${conversationId}`, {
+		const res = await fetch(`https://chat-app-965j.onrender.com/deleteConv/${conversationId}`, {
 			method: 'DELETE',
 			headers: {
 				'Content-Type': 'application/json',
