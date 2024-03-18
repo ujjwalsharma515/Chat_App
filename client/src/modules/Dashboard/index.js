8e4648f6e0357e11557e8e2297b283e6304a0c60
ujjwal <ujjwalsharma515@gmail.com>
2024-03-18 12:47:27 +0530

    delete

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -55,7 +55,7 @@ const Dashboard = () => {
 		fetchConversations()
 	}, [])
 
-	const deleteConv = async (conversationId) => {
+	const deleteConvo = async (conversationId) => {
 		const res = await fetch(`https://chat-app-4-9jcn.onrender.com/api/deleteConv/${conversationId}`, {
 			method: 'DELETE',
 			headers: {
@@ -138,7 +138,7 @@ const Dashboard = () => {
 												<div className='ml-6'>
 													<h3 className='text-lg font-semibold'>{user?.fullName}</h3>
 													<p className='text-sm font-light text-gray-600'>{user?.email}</p>
-													<p className='hover:bg-red-500' onClick={()=> deleteConv(conversationId)}>DEL</p>
+													<p className='hover:bg-red-500' onClick={() => deleteConvo(conversationId)}>DEL</p>
 												</div>
 											</div>
 										</div>
