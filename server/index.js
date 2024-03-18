6afcc63b17d914271c7b109bf4cbe13985266967
ujjwal <ujjwalsharma515@gmail.com>
2024-03-18 13:08:11 +0530

    io

--- server/index.js
+++ server/index.js
@@ -30,6 +30,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/route.htm', function (req, res) {
+    res.sendFile( __dirname + "/" + "route.htm" );
+});
 dotenv.config({path:'./config.env'});
 const port = process.env.PORT || 8000;
 
