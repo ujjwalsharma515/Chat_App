09bad013937b5201dacb618224d56f32fb3bc1c8
ujjwal <ujjwalsharma515@gmail.com>
2024-03-18 13:11:57 +0530

    io

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
 	const messageRef = useRef(null)
 
 	useEffect(() => {
-		setSocket(io('http://localhost:8080'))
+		setSocket(io(`http://localhost:${process.env.SOCKETIOPORT}`))
 	}, [])
 
 	useEffect(() => {
--- server/index.js
+++ server/index.js
@@ -5,17 +5,17 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port1 = process.env.PORT;
 
-const io = require('socket.io')(8080, {
-    cors: {
-        origin: 'http://localhost:3000',
-    }
-});
-
-// const io = require('socket.io')(process.env.SOCKETIOPORT,{
+// const io = require('socket.io')(8080, {
 //     cors: {
-//         origin: `http://localhost:${port1}`,
+//         origin: 'http://localhost:3000',
 //     }
 // });
+
+const io = require('socket.io')(process.env.SOCKETIOPORT,{
+    cors: {
+        origin: `http://localhost:${port1}`,
+    }
+});
 // Connect DB
 require('./db/connection');
 
@@ -30,15 +30,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
-app.get('/route.htm', function (req, res) {
-    res.sendFile( __dirname + "/" + "route.htm" );
-});
 dotenv.config({path:'./config.env'});
 const port = process.env.PORT || 8000;
 
